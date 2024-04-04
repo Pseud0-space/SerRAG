ccfd16396b6727172d52e2db37c0104d825e715c
Arfan Ahmed <82369913+Pseud0-space@users.noreply.github.com>
2024-04-04 14:09:27 +0530

    Add files via upload

---
+++ SerpLocal/globals.py
@@ -0,0 +1,2 @@
+class Vars:
+    scrape = {}
---
+++ SerpLocal/variables.py
@@ -0,0 +1,46 @@
+"""
+This module stores variables utilized within the bot, providing the flexibility to adjust settings as needed.
+"""
+
+
+"""Timeout for page loading in seconds"""
+TIMEOUT_FOR_PAGE_LOAD = 100
+
+
+
+"""default timeout in seconds"""
+DEFAULT_TIMEOUT = 1
+
+
+
+"""
+time to sleep, while scrolling down the window.
+The program will sleep for seconds before continuing scrolling
+(Seconds)
+"""
+SLEEP_TIME = 2
+
+
+
+"""Scrape with headless browser or not
+Headless: True
+Non-Headless: False
+"""
+HEADLESS = False
+
+
+
+"""
+Give proxiy details, like this:
+
+PROXIES = {
+    "server": "http://serveraddress.com:port",
+    "username": "user",
+    "password": "password"
+}
+
+
+If you want to use proxies set its value as none
+"""
+
+PROXIES = None
